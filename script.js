2b13b0c0e267143d9bda922f777b31bbff9fc742
Jamshid Dostonov <Dostonov888@yandex.ru>
2021-01-29 21:47:42 +0300

    Изменил стили добавил 2-домашку

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission = 'The Game Begins!';
-let period;
-alert(mission);
-console.log('Hello World!');
+" use strict ";
+
+let money = 100000,
+    income = 'freelance',
+    addExpenses = 'Taxi, Taxes, Apartment Rentals',
+    deposit = true,
+    mission = 500000,
+    period = 6;
+
+console.log(typeof money, typeof income, typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месецев');
+console.log('Цель заработать ' + mission + ' долларов');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+let budgetDay = (money /= 30);
+
+console.log(budgetDay);

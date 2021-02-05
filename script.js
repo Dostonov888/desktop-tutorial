23417be62beb81ee3ece41e151e683e4af4c31bf
Jamshid Dostonov <Dostonov888@yandex.ru>
2021-02-06 01:26:20 +0300

    Выполнил ДЗ 7-урока

--- script.js
+++ script.js
@@ -1,81 +1,90 @@
 " use strict ";
+
+let money,
+
+    start = function () {
+        do {
+            money = +prompt('Ваш месячный доход?', 50000);
+        }
+        while (isNaN(money) || money === '' || money === null)
+    };
+start();
+
+let appData = {
+    budget: money,
+    budgetDay: 0,
+    budgetMonth: 0,
+    expensesMonth: 0,
+    income: {},
+    addIncome: [],
+    expenses: {},
+    addExpenses: [],
+    deposit: false,
+    mission: 100000,
+    period: 6,
+    asking: function () {
+        let addExpenses = prompt('Перечилите возможные расходы через запятую!'); appData.addExpenses = addExpenses.toLowerCase().split(',');
+        appData.deposit = confirm('Есть ли у вас депозит в банке?');
+    }
+};
+console.log(appData);
+
+appData.getTargetMonth = function () {
+
+    return mission / appData.accumulatedMonth;
+
+};
+let showTypeof = function (item) {
+    console.log(typeof item);
+};
+showTypeof(money);
+showTypeof(appData.income);
+showTypeof(appData.deposit);
+
+
+
+let income = 'freelance',
+    mission = 500000,
+    period = 6;
+// 1-задание
 let isNumber = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
-
-let money,
-    income = 'freelance',
-    deposit = confirm('Есть ли у вас депозит в банке ?'),
-    mission = 500000,
-    period = 6;
-
-let showTypeOf = function (item) {
-    console.log(typeof item);
-};
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-
-
-let start = function () {
-    do {
-        money = +prompt('Ваш месячный доход?');
-    }
-    while (!isNumber(money)); {
-        console.log(money);
-    }
-};
-
-start();
-
-let addExpenses = prompt('Перечилите возможные расходы через запятую!');
-console.log(addExpenses.toLowerCase().split(','));
-
-
-// 1-задание
 let expenses = [];
-let getExpensesMonth = function () {
+appData.getExpensesMonth = function () {
     let sum = 0;
     do {
         expenses[sum] = prompt('Введите обязательную статью расходов?');
         sum = +prompt('Во сколько это обойдется?', 1000);
 
         sum++;
-    } while (!isNumber(sum), sum < 2);
-
-
+    } while (isNumber(sum), sum < 2);
     console.log(expenses);
     return sum;
 };
 
 
-let expensesAmount = getExpensesMonth();
-console.log('Расходы на месец: ' + expensesAmount);
+appData.expensesAmount = appData.getExpensesMonth();
+console.log('Расходы на месец: ' + appData.expensesAmount);
 
 // 2-3-задание
 
-let getAccumulatedMonth = function () {
-    return money - getExpensesMonth();
+appData.getAccumulatedMonth = function () {
+    return money - appData.getExpensesMonth();
 };
 
-let accumulatedMonth = getAccumulatedMonth();
+appData.accumulatedMonth = appData.getAccumulatedMonth();
 
-console.log(accumulatedMonth);
+console.log(appData.accumulatedMonth);
 
 // 4-задание
 
-let getTargetMonth = function () {
 
-    return mission / accumulatedMonth;
+appData.getTargetMonth();
 
-};
-
-getTargetMonth();
-
-if (getTargetMonth() > 0) {
-    console.log('Цель будет достигнута за ' + Math.floor(getTargetMonth()) + ' месяцев(-а)');
-} else if (getTargetMonth() <= 0) {
+if (appData.getTargetMonth() > 0) {
+    console.log('Цель будет достигнута за ' + Math.floor(appData.getTargetMonth()) + ' месяцев(-а)');
+} else if (appData.getTargetMonth() <= 0) {
     console.log('Цель не будет достигнута');
 } else { console.log('код не работает'); }
 
@@ -83,22 +92,22 @@ if (getTargetMonth() > 0) {
 
 
 // удалил budgetMonth
-let budgetDay = accumulatedMonth / 30;
+appData.budgetDay = appData.accumulatedMonth / 30;
 
-console.log('Бюджет на день ' + Math.floor(budgetDay) + ' рублей');
+console.log('Бюджет на день ' + Math.floor(appData.budgetDay) + ' рублей');
 
 let getStatusIncome = function () {
     switch (true) {
-        case (budgetDay) > 1200:
+        case (appData.budgetDay) > 1200:
             console.log('У вас высокий уровень дохода');
             break;
-        case (budgetDay) > 600 && (budgetDay) < 1200:
+        case (appData.budgetDay) > 600 && (budgetDay) < 1200:
             console.log('У вас средний уровень дохода');
             break;
-        case (budgetDay) < 600 && (budgetDay) > 0:
+        case (appData.budgetDay) < 600 && (budgetDay) > 0:
             console.log('К сожалению у вас уровень дохода ниже среднего');
             break;
-        case (budgetDay) > 0 && (budgetDay) === 600 && (budgetDay) < 1200:
+        case (appData.budgetDay) > 0 && (appData.budgetDay) === 600 && (appData.budgetDay) < 1200:
             console.log('К какому уровню не важно');
             break;
         default:
